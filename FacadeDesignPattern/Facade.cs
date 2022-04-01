6266bdd4be41f8d91cc05d7bcb445f0aa19bd729
Uğurcan SANDIK <44526848+ugurcansandik@users.noreply.github.com>
2022-04-01 13:09:21 +0300

    Facade Design Pattern

---
+++ FacadeDesignPattern/Facade.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadeDesignPattern
+{
+    internal class Facade
+    {
+        public IGraphicCard _graphicCard;
+        public IGraphicCardBrand _graphicCardBrand;
+
+        public Facade()
+        {
+            _graphicCard = new GraphicCard();
+            _graphicCardBrand = new GraphicCardBrand();
+        }
+    }
+}
---
+++ FacadeDesignPattern/GraphicCard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadeDesignPattern
+{
+    internal class GraphicCard : IGraphicCard
+    {
+        public void CreateGraphicCard()
+        {
+            //graphic card oluşturulur
+        }
+    }
+}
